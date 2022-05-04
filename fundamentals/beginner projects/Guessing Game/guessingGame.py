88c3f16660cdb665ba8764d9d1b7cad4c13bc284
kdavis73 <kdavis73@asu.edu>
2022-05-04 00:04:43 -0400

    1. add a function for the computer to guess the random number 2. print the number of steps the computer took

--- fundamentals/beginner projects/Guessing Game/guessingGame.py
+++ fundamentals/beginner projects/Guessing Game/guessingGame.py
@@ -1,5 +1,26 @@
 import random
 
+def computerGuess(lowval, highval, randnum, count = 0):
+	if highval >= lowval:
+		guess = lowval + (highval - lowval) // 2
+		# If guess is in the middle, it is found!
+		if guess == randnum:
+			return count
+		# if "guess" is greater than the number,
+		# it must be found in the lower half of the set of numbers
+		# between the lower value and the guess
+		elif guess > randnum:
+			count = count + 1
+			return computerGuess(lowval, guess - 1, randnum, count)
+		else:
+			count = count + 1
+			return computerGuess(guess + 1, highval, randnum, count)
+
+	else:
+		# number not found
+		return -1
+
+
 # generate a random number between 1 and 100
 randnum = random.randint(1, 101)
 
@@ -20,3 +41,4 @@ while(guess != randnum):
 	count = count + 1
 # end of while loop
 print(f"You took {count} steps to guess the number")
+print(f"computer took {computerGuess(0, 100, randnum)} steps!")
