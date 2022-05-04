30146f82e29f1e7c676903ff71e6653cae80cf40
kdavis73 <kdavis73@asu.edu>
2022-05-03 23:23:45 -0400

    1. import random module 2. generate random number 3. cast user input to an integer

--- fundamentals/beginner projects/Guessing Game/guessingGame.py
+++ fundamentals/beginner projects/Guessing Game/guessingGame.py
@@ -1,2 +1,7 @@
+import random
+
 # Get the user's guess
-guess = input("Enter your guess between 1 and 100: ")
+guess = (int) (input("Enter your guess between 1 and 100: "))
+
+# generate a random number between 1 and 100
+randnum = randint(1, 101)
