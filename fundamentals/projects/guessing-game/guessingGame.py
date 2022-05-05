3ebe1bfbd6d2fae95eff5c027bad4b7e95f7ea68
kellzcodes <keldavis5@yahoo.com>
2022-05-04 23:08:36 -0400

    create function to have computer guess random number

--- fundamentals/projects/guessing-game/guessingGame.py
+++ fundamentals/projects/guessing-game/guessingGame.py
@@ -1,5 +1,26 @@
 import random
 
+def computerGuess(lowVal, highVal, randnum, count = 0):
+	if highVal >= lowVal:
+		guess = lowVal + (highVal - lowVal) // 2
+		# If guess is in the middle, it is found!
+		if guess == randnum:
+			return count
+		# If "guess" is greater than the number,
+		# it must be found in the lower half of the set of numbers
+		# between the lower value and the guess
+		elif guess > randnum:
+			count = count + 1
+			return computerGuess(lowVal, guess - 1, randnum, count)
+		# The number must be in the upper set of numbers
+		# between the guess and the upper value
+		else:
+			count = count + 1
+			return computerGuess(guess + 1, highVal, randnum, count)
+	else:
+		# number not found
+		return -1
+
 # generate a random number between 1 and 100
 randnum = random.randint(1, 101)
 
@@ -20,4 +41,5 @@ while(guess != randnum):
 	count = count + 1
 # end of while loop
 
-print(f"You took {count} steps to guess the number")
+print(f"You took {count} steps to guess the number!")
+print(f"The computer took {computerGuess(0, 100, randnum)} steps!")
