9320c283db6938fa7761c7a219d86f695d60d4a4
kellzcodes <keldavis5@yahoo.com>
2022-05-04 22:20:55 -0400

    create basic if else statements

--- fundamentals/projects/guessing-game/guessingGame.py
+++ fundamentals/projects/guessing-game/guessingGame.py
@@ -5,3 +5,10 @@ guess = (int) (input("Enter your guess between 1 and 100: "))
 
 # generate a random number between 1 and 100
 randnum = random.randint(1, 101)
+
+if guess < randnum:
+	print("higher")
+elif guess > randnum:
+	print("lower")
+else:
+	print("you guessed it!")
