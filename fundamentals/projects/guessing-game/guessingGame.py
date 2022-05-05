8f9ab9c270e77927d93f76523393cdffe40d1504
kellzcodes <keldavis5@yahoo.com>
2022-05-04 22:44:50 -0400

    create a while loop

--- fundamentals/projects/guessing-game/guessingGame.py
+++ fundamentals/projects/guessing-game/guessingGame.py
@@ -1,14 +1,23 @@
 import random
 
-# Get the user's guess
-guess = (int) (input("Enter your guess between 1 and 100: "))
-
 # generate a random number between 1 and 100
 randnum = random.randint(1, 101)
 
-if guess < randnum:
-	print("higher")
-elif guess > randnum:
-	print("lower")
-else:
-	print("you guessed it!")
+count = 0
+guess = -99
+
+while(guess != randnum):
+	# Get the user's guess
+	guess = (int) (input("Enter your guess between 1 and 100: "))
+
+	if guess < randnum:
+		print("higher")
+	elif guess > randnum:
+		print("lower")
+	else:
+		print("you guessed it!")
+		break
+	count = count + 1
+# end of while loop
+
+print(f"You took {count} steps to guess the number")
