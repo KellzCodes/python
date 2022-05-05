20e17872d431d5c45b7996cc84ae203fa1533682
kellzcodes <keldavis5@yahoo.com>
2022-05-05 00:04:27 -0400

    Create user input

--- fundamentals/projects/dice-game/diceGame.py
+++ fundamentals/projects/dice-game/diceGame.py
@@ -1,6 +1,16 @@
 import random
 
+def main():
+
+	playerOne = input("Player One - Please enter your name: ")
+	print(f"Hello, {playerOne} \n")
+
+	playerTwo = input("Player Two - Please enter your name: ")
+	print(f"Hello, {playerTwo} \n")
+
 def roll_dice():
 	dice = random.randint(1, 6)
 
 	return dice
+
+main()
