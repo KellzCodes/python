8c5d4b36e43a07e6f65a4a53720ea94faf17fe1f
Kelli Davis <keldavis5@yahoo.com>
2022-05-05 22:56:19 -0400

    Create practice2.py

---
+++ fundamentals/for-loops/practice2.py
@@ -0,0 +1,17 @@
+# use multiple for loops to iterate through the provided list, string, 
+# and tuple, and print all of their elements, each on a separate line
+# first print all of the items, then all of the string characters, 
+# then all of the tuple items
+
+lst = ["tim", "is", "the", "best", "instructor"]
+string = "..."
+tupl = ("and", "he", "is", "great")
+
+for item in lst:
+    print(item)
+
+for character in string:
+    print(character)
+
+for item in tupl:
+    print(item)
