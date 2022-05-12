788e12f001e02a7b932cf14f2ce76a45ed17b244
Kelli Davis <keldavis5@yahoo.com>
2022-05-11 20:48:34 -0400

    Create practice5.py

---
+++ fundamentals/functions/practice5.py
@@ -0,0 +1,13 @@
+# Write a function that takes in a list of strings 
+# and returns a new list containing the lengths of 
+# the strings, in their relevant order. You can 
+# assume that "strings" will only contain strings
+
+def string_lengths(strings):
+    lengths = []
+
+    for string in strings:
+        length = len(string)
+        lengths.append(length)
+
+    return lengths
