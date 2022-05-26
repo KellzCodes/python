50a6b15c39c8fd799f48cc92a0953bfefc5c3b93
Kelli Davis <keldavis5@yahoo.com>
2022-05-25 22:10:31 -0400

    Update hashSolution.py

--- algorithms/arrays/two-number-sum/hashSolution.py
+++ algorithms/arrays/two-number-sum/hashSolution.py
@@ -1,13 +1,9 @@
 def twoNumberSum(array, target):
-    array.sort()
-    left = 0
-    right = len(array) - 1
-    while left < right:
-        current = array[left] + array[right]
-        if current == target:
-            return [array[left], array[right]]
-        elif current < target:
-            left += 1
-        elif current > target:
-            right -= 1
+    nums = {}
+    for num in array:
+        match = target - num
+        if match in nums:
+            return [match, num]
+        else:
+            nums[num] = True
     return []
